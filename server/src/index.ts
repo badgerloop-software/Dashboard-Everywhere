6567d4af5373688693a42fff19fdda08323a3d4e
Eric Udlis <udlis@wisc.edu>
2020-08-27 11:03:56 -0500

    Inital socket.io integration

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+import io from 'socket.io';
+
+const PORT = 3000;
+
+const SERVER = io.listen(PORT);
+
+SERVER.on('connection', (socket) => {
+  console.log('client connected');
+  socket.emit('welcome', 'welcome');
+});
